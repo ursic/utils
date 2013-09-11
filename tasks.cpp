eb3e567beca01ead858fa5f73177eb8835ea02e3
Mitja Uršič <odtihmal@gmail.com>
2013-09-11 18:20:05 +0200

    Tasks (C++).

---
+++ tasks.cpp
@@ -0,0 +1,63 @@
+#include <thread>
+#include <vector>
+#include <algorithm>
+#include "tbb/concurrent_queue.h"
+#include "tasks.hpp"
+
+using namespace std;
+
+tbb::concurrent_queue<thread::id> released_threads;
+vector<thread> threads;
+
+void Threads::store_thread(thread t) {
+    threads.push_back(move(t));
+}
+
+void Threads::release_thread(thread::id id) {
+    released_threads.push(id);
+}
+
+/*
+ * Remove all joined threads
+ * from the threads vector.
+ */
+void Threads::remove_joined_threads() {
+   threads.erase
+        (remove_if(begin(threads),
+                   end(threads),
+                   [&](const thread &t) {
+                       return !t.joinable();
+                   }),
+         end(threads));
+}
+
+/*
+ * Join all yet unjoined threads in the threads vector.
+ * No need to shrink the vector, since this function
+ * is supposed to be called at the end of main.
+ */
+void Threads::join_all_threads() {
+    for (auto &t : threads) {
+        if (t.joinable()) {
+            t.join();
+        }
+    }
+}
+
+/*
+ * Join released threads and clean up the threads vector.
+ * Necessary if the threads vector is filling up.
+ */
+void Threads::join_released_threads() {
+    thread::id t_id;
+    while(0 < released_threads.unsafe_size()) {
+        if (!released_threads.try_pop(t_id)) continue;
+        for (auto &t : threads) {
+            if ((t_id == t.get_id()) &&
+                t.joinable()) {
+                t.join();
+            }
+        }
+    }
+    remove_joined_threads();
+}
---
+++ tasks.hpp
@@ -0,0 +1,34 @@
+/*
+ * clang -std=c++11 -lstdc++ -pthreads -I /tbb/include/ -L /tbb/build/release/ -ltbb tasks.cpp thread.cpp -o thread
+ */
+
+class Threads {
+public:
+    void join_released_threads();
+    void join_all_threads();
+    /*
+     * Start a new thread with given function f and arguments args.
+     * When finished, add the thread's ID to the release_threads queue.
+     * Also, from the main thread, push the thread into
+     * the threads vector so it can be removed once it's released.
+     */
+    template <typename F, typename ...Args>
+    void start_thread(F&& f, Args&&... args) {
+        std::thread t = std::thread([&]{
+                f(args...);
+                release_thread(std::this_thread::get_id());
+            });
+        store_thread(move(t));
+    }
+    ~Threads() {
+        join_all_threads();
+    }
+
+private:
+    void release_thread(std::thread::id);
+    void store_thread(std::thread);
+    void remove_joined_threads();
+};
+
+
+
